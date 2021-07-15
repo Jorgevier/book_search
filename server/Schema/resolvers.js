80ac4ae00442eed181b0fc536097425e6b70dbe8
Jorge Viera <Jorge5psgt@gmail.com>
2021-07-14 23:56:14 -0400

    fixed signup bug

---
+++ server/Schema/resolvers.js
@@ -0,0 +1,68 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) =>{
+      if (context.user){
+        const userData = await User.findOne({})
+        .select('-_v -password')
+        .populate('books')
+        return userData;
+      }
+      throw new AuthenticationError('You are not logged in')
+    },  
+    },
+Mutation:{
+    addUser: async (parent, args) => {
+      try {
+      const user = await User.create(args);
+      const token = signToken(user); //problem with token
+  return { token, user };
+      } catch (e){console.log(e)}
+  },
+login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email }
+        );
+if (!user) {
+          throw new AuthenticationError('Incorrect username or password');
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+          throw new AuthenticationError('Incorrect password');
+      }
+      const token = signToken(user);
+      return { token, user };
+
+  },
+
+  saveBook: async (parent, args, context) => {
+      if (context.user) {
+      const updateUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $addToSet: { savedBooks: args.input } },
+              { new: true }
+          );
+    return updateUser;
+      }
+
+      throw new AuthenticationError('Please log In!');
+  },
+  removeBook: async (parent, args, context) => {
+      if (context.user) {
+          const updateUser = await User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId: args.bookId } } },
+              { new: true }
+          );
+          return updateUser;
+      }
+      throw new AuthenticationError('You need to logged in!');
+  }
+}
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express')
+const path = require('path');
+const { typeDefs, resolvers } = require('./Schema');
 const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('../Schema');
+const db = require('./config/connection');
+// const routes = require('./routes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
 
 server.applyMiddleware({ app });
 
@@ -28,9 +31,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+ app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
